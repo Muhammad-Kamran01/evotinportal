4abb55cfd3e630cac33626087916079454c112c5
Muhammad Kamran <164922304+Muhammad-Kamran01@users.noreply.github.com>
2025-06-19 18:15:37 +0500

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,11 @@
 <?php
+
+session_start([
+    'cookie_secure' => true,
+    'cookie_httponly' => true,
+    'use_strict_mode' => true
+]);
+
 $conn = mysqli_connect("localhost", "root", "", "votingsystem");
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
